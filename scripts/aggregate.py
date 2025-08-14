44630f16c28aec3788e4d6d9ad770b50ae5cf901
Kentaro Mizuki <66548698+harsssh@users.noreply.github.com>
2025-08-14 14:02:43 +0900

    3人想定

--- scripts/aggregate.py
+++ scripts/aggregate.py
@@ -217,8 +217,7 @@ def make_heatmap_svg(daily: dict, days: int = 365) -> str:
             return "#c6e48b"
         if v == 2:
             return "#7bc96f"
-        if v == 3:
-            return "#239a3b"
+        # 3人以上
         return "#196127"
 
     svg = [
